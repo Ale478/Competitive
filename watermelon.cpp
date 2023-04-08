97530616ba1eb1aa20e7bd25d289972f4bdcc13b
Ale478 <alemusicmolin@gmail.com>
2023-04-07 21:13:36 -0300

    add watermelon exercise

---
+++ watermelon.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+    int w;
+    cin >> w;
+
+    // w is an even number 
+    if((w % 2 == 0)&&(w != 2)){
+        cout << "YES" << endl;
+    }
+
+    // w is an odd number
+    else{
+        cout << "NO" << endl;
+    }
+}
